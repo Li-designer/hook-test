1b2435780c76aa6c6dfda72ffea8514f79377076
lj <51325lj@sina.com>
2025-01-13 10:49:22 +0800

    feat: 定时器的封装

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App9.tsx";
+import App from "./App10.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as any).render(<App />);
 
